4547c20dc0f8981e164609219f6876f3ee74bfa2
pedrohcalado <pedrocalado22@gmail.com>
2021-11-18 09:52:04 -0300

    Project7

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -9,23 +9,89 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    var urlString: String?
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
-        let urlString = "https://pokeapi.co/api/v2/ability/?limit=20&offset=20"
         
-        if let url = URL(string: urlString) {
-//            print(url)
-            if let data = try? Data(contentsOf: url) {
-                print(data)
-                parse(json: data)
+        // 1
+        searchController.searchResultsUpdater = self
+        // 2
+        searchController.obscuresBackgroundDuringPresentation = false
+        // 3
+        searchController.searchBar.placeholder = "Search petitions"
+        // 4
+        navigationItem.searchController = searchController
+        // 5
+        definesPresentationContext = true
+
+        
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(creditsTapped))
+
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+//            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=1000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+//        let urlString = "https://pokeapi.co/api/v2/ability/?limit=20&offset=20"
+        
+        if let urlString = urlString {
+            if let url = URL(string: urlString) {
+             
+                if let data = try? Data(contentsOf: url) {
+                    print(data)
+                    parse(json: data)
+                    return
+                }
+                showError()
             }
         }
         
+        
+        
     }
     
+    func filterContentForSearchText(_ searchText: String) {
+      filteredPetitions = petitions.filter { (petition: Petition) -> Bool in
+          return petition.title.lowercased().contains(searchText.lowercased())
+      }
+      
+      tableView.reloadData()
+    }
     
+    var isSearchBarEmpty: Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    var isFiltering: Bool {
+      return searchController.isActive && !isSearchBarEmpty
+    }
+
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "there was an error", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(ac, animated: true, completion: nil)
+    }
+    
+    @objc func creditsTapped() {
+        guard let url = urlString else {
+            return
+        }
+
+        let ac = UIAlertController(title: "Credits", message: "\(url)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .cancel))
+        present(ac, animated: true, completion: nil)
+    }
     
     func parse(json: Data) {
         let decoder = JSONDecoder()
@@ -37,21 +103,51 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        if isFiltering {
+            return filteredPetitions.count
+          }
+          return petitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         var content = cell.defaultContentConfiguration()
-        let petition = petitions[indexPath.row]
+        let petition: Petition
+        
+        if isFiltering {
+            petition = filteredPetitions[indexPath.row]
+        } else {
+            petition = petitions[indexPath.row]
+        }
+    
         content.text = petition.title
         content.secondaryText = petition.body
         
-        cell.contentConfiguration = content
+        content.secondaryTextProperties.numberOfLines = 2
         
+        cell.contentConfiguration = content
         
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        if isFiltering {
+            vc.detailItem = filteredPetitions[indexPath.row]
+        } else {
+            vc.detailItem = petitions[indexPath.row]
+        }
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
+
+extension ViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+      let searchBar = searchController.searchBar
+      filterContentForSearchText(searchBar.text!)
+  }
+}
+
 
